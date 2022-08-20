ffaa01216f93ce03c4373778360e0eb02cd0fbb0
marciovz <marciovz@yahoo.com.br>
2022-08-20 20:49:41 -0300

    configuração do eslint e correções automaticas de escrita

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonContainerProps {
   variant: ButtonVariant
@@ -13,7 +13,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme['green-500']};
-  color: ${ props => props.theme.white};
-
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 `
